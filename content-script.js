9b55764f6ecb06bcb8f60df05f66a89b8d9d83bf
jhamadhav <jhamadhav28@gmail.com>
2020-07-23 15:28:45 +0530

    major patch

--- content-script.js
+++ content-script.js
@@ -5,93 +5,38 @@ console.log("successfully injected a code");
 // true by default
 let is_allowed = true;
 
+// function to check url
+const validURL = (str) => {
+    var pattern = new RegExp('^(https:\\/\\/){1}' + // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+        '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+    return !!pattern.test(str);
+}
+
 // the anchor tag and function to show it
 let a = document.getElementsByTagName("a");
 // show function
 const show = (e) => {
     // the url from anchor tab
-    let url = e.target.href;
-    console.log(url);
+    let url = e.target.parentNode.href || e.target.href;
+    // console.log(url);
+    if (validURL(url)) {
+        pop_msg(url, "#0abf53");
+    }
 
-    // the styling
-    let style = document.createElement('style');
-    let style_data = `.msg-popup {
-        position: fixed;
-        display: -webkit-box;
-        display: -ms-flexbox;
-        display: flex;
-        -webkit-box-orient: horizontal;
-        -webkit-box-direction: normal;
-            -ms-flex-direction: row;
-                flex-direction: row;
-        -webkit-box-pack: justify;
-            -ms-flex-pack: justify;
-                justify-content: space-between;
-        -webkit-box-align: center;
-            -ms-flex-align: center;
-                align-items: center;
-        -ms-flex-line-pack: center;
-            align-content: center;
-        bottom: 30px;
-        left: 40px;
-        z-index: 2;
-        background: #262626;
-        border-radius: 8px;
-        font-size: medium;
-        max-width: 440px;
-        color: white;
-        border-left: 18px solid red;
-        -webkit-animation: popup-anim 5s linear;
-                animation: popup-anim 5s linear;
-      }
-      
-      .msg-popup .popup-txt {
-        padding: 10px 14px 10px 14px;
-      }
-      
-      @keyframes popup-anim {
-        0% {
-          -webkit-transform: translate(-100%);
-                  transform: translate(-100%);
-        }
-        10% {
-          -webkit-transform: translate(0);
-                  transform: translate(0);
-        }
-        90% {
-          -webkit-transform: translate(0);
-                  transform: translate(0);
-        }
-        100% {
-          -webkit-transform: translate(-100%);
-                  transform: translate(-100%);
-        }
-      }`;
-
-    // deploying style 
-    style.innerHTML = style_data;
-
-    // Get the first script tag
-    var ref = document.querySelector('script');
-
-    // Insert our new styles before the first script tag
-    ref.parentNode.insertBefore(style, ref);
-
-    pop_msg(url, "#0abf53");
 }
 
 // get all anchor tags and add a event listener so the the link that they point to is console logged
 const set_event = () => {
-    for (let i = 0; i < a.length; i++) {
-        a[i].addEventListener("mouseover", show);
-    }
+    document.addEventListener("mouseover", show);
 }
 
 // remove event
 const unset_event = () => {
-    for (let i = 0; i < a.length; i++) {
-        a[i].removeEventListener("mouseover", show);
-    }
+    document.removeEventListener("mouseover", show);
 }
 // call since it should be on by default
 set_event();
@@ -114,6 +59,10 @@ browser.runtime.onMessage.addListener((message) => {
 
 // popup message 
 function pop_msg(txt, col) {
+
+    // delete previous popup when a new comes up
+    delete_all_popup();
+
     let popup_outer = document.createElement("div");
     let popup_inner = document.createElement("div");
     popup_outer.classList = 'msg-popup';
@@ -135,4 +84,13 @@ function pop_msg(txt, col) {
         document.body.removeChild(popup_outer);
     });
 
+}
+
+// delete all present 
+const delete_all_popup = () => {
+    let popup = document.getElementsByClassName("msg-popup");
+
+    for (let i = 0; i < popup.length; i++) {
+        popup[i].remove();
+    }
 }
